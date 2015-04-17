cafc13f27b440fc915b04ca1ee1b1f6aa08ff6dc
Stepan Snigirev <snigirev.stepan@gmail.com>
2015-04-17 23:19:26 +0300

    kinda works
    Signed-off-by: Stepan Snigirev <snigirev.stepan@gmail.com>

--- wlm.py
+++ wlm.py
@@ -17,26 +17,27 @@ def GetExposureMode(dll):
 def main():
   """
   This function should be called first to establish connection with wmlData.dll library
-  Argument: path to the library
   Returns:  link to the library
   """
   #dllname = 'C:\Windows\System32\wmlData.dll'
-  args = sys.argv[1:]
-  if not len(args):
-    print 'usage: wlm.py path_to_wlmData.dll'
-    sys.exit(1)
+  #args = sys.argv[1:]
+  #if not len(args):
+  #  print 'usage: wlm.py path_to_wlmData.dll'
+  #  sys.exit(1)
 
-  dllname = args[0]
+  #dllname = args[0]
 
-  if not os.path.isfile(dllname):
-    print 'cant find the dll, check the path:', dllname
-    sys.exit(1)
+  #if not os.path.isfile(dllname):
+  #  print 'cant find the dll, check the path:', dllname
+  #  sys.exit(1)
 
-  dllname = os.path.abspath(dllname)
+  #dllname = os.path.abspath(dllname)
+  dllname = "C:\wlmData.dll"
   print 'loading dll:', dllname
 
   dll = ctypes.WinDLL( dllname )
-  return dll
+  print GetExposureMode(dll)
+#  return dll
 
 if __name__ == "__main__":
   main()
